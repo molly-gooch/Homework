b18b7916ec4fdd0d95f91e78767bb1c6c3836b4a
Molly Gooch <molly_gooch26@milton.edu>
2023-10-25 11:12:37 -0400

    Made a change

--- src/B3_HalfTheRed.java
+++ src/B3_HalfTheRed.java
@@ -2,7 +2,7 @@ public class B3_HalfTheRed extends World {
     public int red;
     public int blue;
     public int green;
-
+//change
     public void go() {
         System.out.println("This will be printed to the  window. ");
         plane.teleport(0, 0);
